57c655233daaa6ae3a99daaae92d519948a34c12
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-20 21:21:31 +0530

    added try catch block

--- Main.java
+++ Main.java
@@ -6,27 +6,32 @@ import correcter.encoding.character.*;
 
 public class Main {
     public static void main(final String[] args) {
-        if (args.length == 0)
-            System.out.println("No encoding mode added.");
-        else {
-            switch (args[0].toLowerCase()) {
-                case "hamming":
-                    EncodeHamming.all();
-                    break;
-                case "bit":
-                    EncodeBit.all();
-                    break;
-                case "char":
-                    EncodeChar.all();
-                    break;
-                default:
-                    wrongMode();
+        try {
+            if (args.length == 0) {
+                System.out.println("No encoding mode added.");
+                wrongMode();
+            } else {
+                switch (args[0].toLowerCase()) {
+                    case "hamming":
+                        EncodeHamming.all();
+                        break;
+                    case "bit":
+                        EncodeBit.all();
+                        break;
+                    case "char":
+                        EncodeChar.all();
+                        break;
+                    default:
+                        System.out.println("Incorrect encoding mode");
+                        wrongMode();
+                }
             }
+        } catch (Exception e) {
+            System.out.println("Error");
         }
     }
 
     private static void wrongMode() {
-        System.out.println("Incorrect encoding mode");
         System.out.println("Choose From:");
         System.out.println("hamming");
         System.out.println("bit");
